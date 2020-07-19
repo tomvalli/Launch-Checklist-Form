2d590ee5bbf0a9dd82a091b4351537ed39fbd981
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-07-19 01:10:09 -0500

    updated script.js with form validation - incomplete.

--- script.js
+++ script.js
@@ -11,3 +11,30 @@
 </ol>
 <img src="${}">
 */
+
+function init() {
+   let button = document.getElementById("formSubmit");
+   let form = document.getElementById("launchForm");
+
+   button.onclick = ()=> {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      let formInput = [pilotName, copilotName, fuelLevel, cargoMass];
+
+      for (let i = 0; i < formInput.length; i++) {
+         let fieldName = formInput[i].parentNode.innerHTML;
+         fieldName = fieldName.slice(0,fieldName.indexOf("<")).trim();
+         if (formInput[i].value === "") {
+            form.innerHTML += `<p style='color: red' class='errors'>- 
+               ${fieldName} is required</p>`;
+            event.preventDefault();
+         }
+      }
+
+      
+   };
+}
+
+window.onload = init;
