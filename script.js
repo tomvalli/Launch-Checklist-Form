816a3d782c19ce3d8ad3034e3c0f74be1f5d968b
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-07-22 19:23:46 -0500

    updated script.js to handle form validation. Manipulating DOM in progress

--- script.js
+++ script.js
@@ -16,22 +16,56 @@ function init() {
    let button = document.getElementById("formSubmit");
    let form = document.getElementById("launchForm");
 
+
+
    button.onclick = ()=> {
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      let formInput = [pilotName, copilotName, fuelLevel, cargoMass];
-
-      for (let i = 0; i < formInput.length; i++) {
-         let fieldName = formInput[i].parentNode.innerHTML;
-         fieldName = fieldName.slice(0,fieldName.indexOf("<")).trim();
-         if (formInput[i].value === "") {
-            form.innerHTML += `<p style='color: red' class='errors'>- 
-               ${fieldName} is required</p>`;
-            event.preventDefault();
-         }
+      
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         window.alert("All fields are required.");
+         event.preventDefault();
+         return;
       }
+      fuelLevel = Number(fuelLevel.value);
+      cargoMass = Number(cargoMass.value);
+      if (isNaN(fuelLevel)) {
+         window.alert("Fuel level must be a number");
+      } else if (isNaN(cargoMass)){
+         window.alert("Cargo mass must be a number");
+      }
+      let faultyItems = document.getElementById("faultyItems");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let launchStatus = document.getElementById("launchStatus");
+
+      pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch.`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch`;
+
+      if (fuelLevel < 10000) {
+         faultyItems.style.visibility = "visible";
+         fuelStatus.innerHTML = "Fuel level too low for launch.";
+         launchStatus.innerHTML = "Shuttle not ready for launch.";
+         launchStatus.style.color = red;
+      } else if (cargoMass > 10000) {
+         faultyItems.style.visibility = "visible";
+         cargoStatus.innerHTML = "Cargo mass is to great for launch.";
+         launchStatus.innerHTML = "Shuttle not ready for launch.";
+         launchStatus.style.color = red;
+      } else {
+         launchStatus.innerHTML = "Shuttle is ready for launch.";
+         launchStatus.style.color = green;
+      }
+
+
+      
+
+
+
 
       
    };
